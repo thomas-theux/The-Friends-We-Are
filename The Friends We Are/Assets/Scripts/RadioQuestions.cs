8b61e2dcccc40ca50af96f26dc4ce0eaf3dca1f3
Thomas Theux <t.grikschas@gmail.com>
2018-10-25 21:37:38 +0200

    Designed Questions Review screens

--- The Friends We Are/Assets/Scripts/RadioQuestions.cs
+++ The Friends We Are/Assets/Scripts/RadioQuestions.cs
@@ -14,6 +14,7 @@ public class RadioQuestions : MonoBehaviour {
 		}
 	}
 
+
 	private void Start() {
 		RadioManager.questionsArr = new List<RadioQuestion> {
 
@@ -80,104 +81,3 @@ public class RadioQuestions : MonoBehaviour {
 		};
 	}
 }
-
-
-
-
-
-
-
-	// public static List<string[]> testList = new List<string[]>();
-
-
-	// private void Start() {
-
-	// 	for (int i = 0; i < 6; i++) {
-	// 		Radio.questionsArr.Add(new List<string>());
-	// 	}
-
-	// 	Radio.questionsArr[0].AddRange(new string[] {
-	// 		"If you find out that a very close friend has incurable cancer – what would you do?",
-	// 		"Spend more time with her/him.",
-	// 		"Don’t spend any time anymore.",
-	// 		"Slowly distance myself from her/him.",
-	// 		"I would not change anything."
-	// 	});
-
-	// 	Radio.questionsArr[1].AddRange(new string[] {
-	// 		"Which is your favorite season?",
-	// 		"Spring",
-	// 		"Summer",
-	// 		"Fall",
-	// 		"Winter"
-	// 	});
-
-	// 	Radio.questionsArr[2].AddRange(new string[] {
-	// 		"What is Player One's best attribute of the followoing?",
-	// 		"Punctuality",
-	// 		"Discipline",
-	// 		"Wisdom",
-	// 		"Tolerance"
-	// 	});
-
-	// 	Radio.questionsArr[3].AddRange(new string[] {
-	// 		"Which one of you is the party dude/dudess?",
-	// 		"Player One",
-	// 		"Player Two",
-	// 		"Both of us",
-	// 		"No one"
-	// 	});
-
-	// 	Radio.questionsArr[4].AddRange(new string[] {
-	// 		"Who was better back in school?",
-	// 		"Player One",
-	// 		"Player Two",
-	// 		"We were both awful..",
-	// 		"We both rocked!"
-	// 	});
-
-	// 	Radio.questionsArr[5].AddRange(new string[] {
-	// 		"Who was better back in school?",
-	// 		"Player One",
-	// 		"Player Two",
-	// 		"We were both awful..",
-	// 		"We both rocked!"
-	// 	});
-
-
-	// 	// Radio.questionsArr.AddRange(new string[] {
-	// 	// 	"If you find out that a very close friend has incurable cancer – what would you do?",
-	// 	// 	"Which is your favorite season?",
-	// 	// 	"Which one of you is the party dude/dudess?",
-	// 	// 	"Who was better back in school?",
-	// 	// 	"What fruit do you prefer?"
-	// 	// });
-
-	// 	// Radio.answersArr.AddRange(new string[] {
-	// 	// 	"Spend more time with her/him." + "\n" +
-	// 	// 	"Don’t spend any time anymore." + "\n" +
-	// 	// 	"Slowly distance myself from her/him." + "\n" +
-	// 	// 	"I would not change anything.",
-
-	// 	// 	"Spring" + "\n" +
-	// 	// 	"Summer" + "\n" +
-	// 	// 	"Fall" + "\n" +
-	// 	// 	"Winter",
-
-	// 	// 	"Player One" + "\n" +
-	// 	// 	"Player Two" + "\n" +
-	// 	// 	"Both of us" + "\n" +
-	// 	// 	"No one",
-
-	// 	// 	"Player One" + "\n" +
-	// 	// 	"Player Two" + "\n" +
-	// 	// 	"We were both awful.." + "\n" +
-	// 	// 	"We both rocked!",
-
-	// 	// 	"Apples" + "\n" +
-	// 	// 	"Bananas" + "\n" +
-	// 	// 	"Strawberries" + "\n" +
-	// 	// 	"Melons"
-	// 	// });
-		
-	// }
